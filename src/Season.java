b34a9cf3fe120bf748a91ed627fd64ffb99e472c
Robert Piwowarczyk <robpiw@op.pl>
2020-05-31 17:45:22 +0200

    Cztery pory roku

---
+++ src/Season.java
@@ -0,0 +1,17 @@
+public enum Season {
+    SPRING("Wiosna"),
+    SUMMER("Lato"),
+    AUTUMN("Jesień"),
+    WINTER("Zima");
+
+    private String translation;
+
+    Season(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+}
+
